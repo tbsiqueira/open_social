9d953a175f53428b7357d33cdf77f7351b12a1b8
Oleksandr Maksymiuk <alexander.maximiuk@onbox.app>
2022-02-01 21:35:23 +0200

    Issue #3232848 by alex.ksis: Fix phpcs warnings.

--- tests/behat/features/bootstrap/EmailContext.php
+++ tests/behat/features/bootstrap/EmailContext.php
@@ -78,7 +78,7 @@ class EmailContext implements Context {
       $finder->files()->in($spoolDir);
       return $finder;
     }
-    catch (InvalidArgumentException $exception) {
+    catch (\InvalidArgumentException $exception) {
       return NULL;
     }
   }
@@ -104,6 +104,7 @@ class EmailContext implements Context {
    */
   protected function getSpoolDir() {
     $path = DRUPAL_ROOT . '/' . drupal_get_path('profile', 'social') . '/tests/behat/features/symfony-mailer-spool';
+
     if (!file_exists($path)) {
       mkdir($path, 0777, true);
     }
